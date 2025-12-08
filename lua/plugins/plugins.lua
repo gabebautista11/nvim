53972bd3044c2ded6a78d64841d25fca32634d90
Gabe Bautista <gbautista@alta.com>
2025-12-08 15:41:01 -0700

    added comments

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -194,10 +194,14 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons" },
 		opts = {},
 	},
+
+	------------------------------------------------------------
+	--- GIT INTEGRATION
+	------------------------------------------------------------
 	{ "lewis6991/gitsigns.nvim" },
 
 	------------------------------------------------------------
-	-- TELESCOPE
+	-- NAVIGATION
 	------------------------------------------------------------
 	{
 		"nvim-telescope/telescope.nvim",
