4f91ec96208b32520d557b638bdb07878d657d84
Gabe Bautista <gbautista@alta.com>
2025-12-09 15:21:19 -0700

    Set some defaults for tabs and spaces

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 require("config.lazy")
---this is an auto save text
 
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
 vim.o.number = true
 vim.o.relativenumber = true
