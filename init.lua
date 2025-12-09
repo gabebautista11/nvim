22331b66ea11dc3c074b773b107c01a4a33663ee
Gabe Bautista <gbautista@alta.com>
2025-12-09 14:07:02 -0700

    added auto save plugin

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
+--this is an auto save text
 
 vim.o.number = true
 vim.o.relativenumber = true
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -138,6 +138,19 @@ return {
 	},
 
 	------------------------------------------------------------
+	---AUTO SAVE
+	------------------------------------------------------------
+	{
+		"okuuva/auto-save.nvim",
+		version = "^1.0.0", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+		cmd = "ASToggle", -- optional for lazy loading on command
+		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+		opts = {
+			-- your config goes here
+			-- or just leave it empty :)
+		},
+	},
+	------------------------------------------------------------
 	--- GIT INTEGRATION
 	------------------------------------------------------------
 	{ "lewis6991/gitsigns.nvim" },
