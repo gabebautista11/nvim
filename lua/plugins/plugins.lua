141bd73bf2eca86a0ed52dce99b83f3ecaf4cace
Gabe Bautista <gbautista@alta.com>
2025-12-10 10:19:12 -0700

    added Trouble and status line

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -100,6 +100,47 @@ return {
 	},
 
 	------------------------------------------------------------
+	-- TROUBLE
+	------------------------------------------------------------
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+
+	------------------------------------------------------------
 	-- TREE-SITTER
 	------------------------------------------------------------
 	{
@@ -134,15 +175,9 @@ return {
 		end,
 	},
 	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		lazy = true,
-		cmd = "Neotree",
-		keys = { { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Open file [e]xplore" } },
-		dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons" },
-		opts = {},
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
-
 	------------------------------------------------------------
 	---AUTO SAVE
 	------------------------------------------------------------
@@ -185,6 +220,15 @@ return {
 				desc = "[s]earch [f]iles",
 			},
 		},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		lazy = true,
+		cmd = "Neotree",
+		keys = { { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Open file [e]xplore" } },
+		dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons" },
+		opts = {},
 	},
 
 	------------------------------------------------------------
