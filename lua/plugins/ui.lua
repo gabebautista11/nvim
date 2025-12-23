11ebd701b7dc62230353d57052342d99350fbbee
Gabe-Bautista <gabe.bautista1115@gmail.com>
2025-12-23 09:27:36 -0700

    added color scheme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,10 +7,17 @@ return {
 			vim.cmd.colorscheme("tokyonight")
 		end,
 	},
+    {
+    -- lua/plugins/rose-pine.lua
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end
+},
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
 		opts = {},
 	},
-	
 }
