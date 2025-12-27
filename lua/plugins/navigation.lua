6ff0d60b4c57462599805fef8713ea827080c11c
Gabe Bautista <gbautista@alta.com>
2025-12-27 11:50:39 -0700

    added a buffer line manager. we will see how it goes

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -61,4 +61,16 @@ return {
 		},
 		lazy = false, -- neo-tree will lazily load itself
 	},
+	{
+		"EL-MASTOR/bufferlist.nvim",
+		lazy = true,
+		keys = { { "<Leader>b", ":BufferList<CR>", desc = "Open bufferlist" } },
+		dependencies = "nvim-tree/nvim-web-devicons",
+		cmd = "BufferList",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,6 +2,15 @@ return {
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
 		opts = {
 			spec = {
 				{ "<leader>f", group = "[f]ind" }, -- 👈 THIS is the key part
