773a2fa2d280d0f55316f9d046cd9210ae42870f
Gabe Bautista <gbautista@alta.com>
2026-01-19 10:19:52 -0700

    removed my own package

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -3,12 +3,4 @@ return {
 		"akinsho/toggleterm.nvim",
 		opts = {},
 	},
-
-	{
-		"gabebautista11/runner.nvim",
-		lazy = false,
-		config = function()
-			require("runner").setup()
-		end,
-	},
 }
