2dec3d75fd4e578cd38eb20b39fd4cc8cc6371f9
gabebautista11 <gabe.bautista1115@gmail.com>
2026-05-11 11:33:27 -0600

    merged some files and trying to get treesitter manager to work

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"dgagn/diagflow.nvim",
-		even = "LspAttach",
+		event = "LspAttach",
 		opts = {},
 	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,67 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	opts = {},
-	dependencies = {
-		{"mason-org/mason.nvim", opts = {}},
-		"neovim/nvim-lspconfig",
+	-- LSP management
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+			},
+			automatic_installation = true,
+
+		},
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				opts = {
+					registries = {
+						"github:mason-org/mason-registry",
+						"github:Crashdummyy/mason-registry",
+					},
+				}
+			},
+			"neovim/nvim-lspconfig",
+
+		},
+	},
+
+	--Formatting Mangement
+	{
+		'stevearc/conform.nvim',
+		opts = {
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+
+			}
+		},
 
 	},
+
+	--Syntax
+	{
+		"romus204/tree-sitter-manager.nvim",
+		dependencies = {}, -- tree-sitter CLI must be installed system-wide
+		config = function()
+			require("tree-sitter-manager").setup({
+
+				-- Default Options
+				ensure_installed = {
+				},
+				auto_install = true,
+			})
+		end
+	},
+
+
+	-- language additions
+	{
+		"seblyng/roslyn.nvim",
+		---@module 'roslyn.config'
+		---@type RoslynNvimConfig
+		opts = {
+			-- your configuration comes here; leave empty for default settings
+		},
+	}
+
+
 }
