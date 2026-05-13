71bab2568d00a73b00f55df6942200f3b7dc8095
gabebautista11 <gabe.bautista1115@gmail.com>
2026-05-13 08:03:17 -0600

    changing the screen loading

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -25,6 +25,43 @@ return {
 	{
 		'leo-alvarenga/homecoming.nvim',
 		branch = 'nightly', -- Optional: specify the nightly branch if you want to use the latest features
-		opts = {}, -- No configuration is required to get started
+		opts = {
+			header = { "Welcome" },
+			header_centered = true,
+			sections = {
+				{
+					title = "Actions",
+					items = {
+						{
+							label = "Explore",
+							action = function()
+								vim.cmd("enew")
+								vim.cmd("Explore")
+							end,
+						},
+
+					},
+				},
+				{
+					title = "Configuration",
+					items = {
+						{
+							label = "Neovim Configuration",
+							action = function()
+								vim.cmd.Explore(vim.fn.stdpath("config"))
+							end,
+						},
+					}
+				},
+
+			},
+
+		}, -- No configuration is required to get started
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
 	},
 }
