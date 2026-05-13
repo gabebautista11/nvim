e316445aa6d32fd9d55332f0f243366fd65c76f2
gabebautista11 <gabe.bautista1115@gmail.com>
2026-05-13 15:35:43 -0600

    added oil and made it work

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -18,6 +18,45 @@ return {
 		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 		lazy = false,
-	}
+	},
+	{
+		'leo-alvarenga/homecoming.nvim',
+		branch = 'nightly', -- Optional: specify the nightly branch if you want to use the latest features
+		dependencies = {
+			"stevearc/oil.nvim",
+
+		},
+		opts = {
+			header = { "Welcome" },
+			header_centered = true,
+			sections = {
+				{
+					title = "Actions",
+					items = {
+						{
+							label = "Explore",
+							action = function()
+								require("oil").open()
+							end,
+						},
+
+					},
+				},
+				{
+					title = "Configuration",
+					items = {
+						{
+							label = "Neovim Configuration",
+							action = function()
+								require("oil").open(vim.fn.stdpath("config"))
+							end,
+						},
+					}
+				},
+
+			},
+
+		},
+	},
 
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -26,42 +26,7 @@ return {
 			}
 		end,
 	},
-	{
-		'leo-alvarenga/homecoming.nvim',
-		branch = 'nightly', -- Optional: specify the nightly branch if you want to use the latest features
-		opts = {
-			header = { "Welcome" },
-			header_centered = true,
-			sections = {
-				{
-					title = "Actions",
-					items = {
-						{
-							label = "Explore",
-							action = function()
-								vim.cmd("enew")
-								vim.cmd("Explore")
-							end,
-						},
 
-					},
-				},
-				{
-					title = "Configuration",
-					items = {
-						{
-							label = "Neovim Configuration",
-							action = function()
-								vim.cmd.Explore(vim.fn.stdpath("config"))
-							end,
-						},
-					}
-				},
-
-			},
-
-		},
-	},
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
